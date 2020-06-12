2c2a6831b02052d60a204d7fdde157a5bd2eb140
trashbirdecology <jessicaleighburnett@gmail.com>
2020-06-12 08:05:41 -0600

    Add roxy doc for groundfish and update .rda with new naming convention

--- data-raw/groundfish-data.R
+++ data-raw/groundfish-data.R
@@ -1,16 +1,26 @@
-## This script prepares the `groundfish` datasets for use in R package `regime-shifts/abrupt``
+## This script  prepares the 'groundfish' data
 
 # Import the original data ---------------------------------------------------------
-## Preferably, the data will be imported via URL and download from a permanent archive within a trusted digital repository
-## However, if this is not possible, please add the dataset to directory /data-raw/ as .csv or .txt
 gf_spatial_info <- read.csv("data-raw/groundfish_spatial_info.csv") # spatial information
 gf_spatiotemporal <- read.csv("data-raw/groundfish_spatiotemporal.csv") # spatiotemporal
 
+
+# Munging -----------------------------------------------------------------
 # append the spatial information to the spatiotemporal data
-groundfish <- merge(gf_spatial_info, gf_spatiotemporal)
+groundfish <- merge(gf_spatial_info, gf_spatiotemporal) %>% as_tibble()
 
-#
 
-# save the data as .rda in directory ./data/
+# Naming convention -------------------------------------------------------
+groundfish %>%
+  dplyr::rename(time=year,
+         value=weight_kg,
+         long=longitude,
+         lat=latitude,
+         site=stratum,
+         variable=taxa_name
+         )
+
+# Save the data as .rda ---------------------------------------------------
 usethis::use_data(groundfish, overwrite = TRUE)
+
 
