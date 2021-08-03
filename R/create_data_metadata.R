fcf0136c02131ff81cb815c8f61569d556709efe
jburnett <jburnett@usgs.gov>
2021-08-03 12:49:25 -0600

    toying around with eml and emld

--- R/create_data_metadata.R
+++ R/create_data_metadata.R
@@ -1,1 +1,20 @@
-## In dev script
+#' @title A Function to Help User Create Data Metadata
+#' @description In dev script to see about automating some of the metadata creationm using EML
+#' @import EML
+#'
+#'
+
+metadat.path <- "~./data/metadata"
+
+auth <- list(individualName = list(givenName = "FirstName", surName = "LastName"))
+my_eml <- list(dataset = list(
+  title = "A Minimal Valid EML for Test Dataset",
+  creator = auth,
+  contact = auth)
+)
+
+
+write_eml(my_eml, "ex.xml")
+#> NULL
+eml_validate("ex.xml")
+
